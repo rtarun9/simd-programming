ddbdb3d1fac7755211d4637f9e5ab9f51070dfb2
rtarun9 <rtarun0913@gmail.com>
2024-08-28 14:24:21 +0530

    Switch to aligned malloc instead of malloc, load array elements into register directly rather than using mm_set_epi32

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-cl /DSIMD_DEBUG=0 /TC /Fe"array_sum.exe" /O1   src/array_sum.c 
+cl /DSIMD_DEBUG=0 /TC /Fe"array_sum.exe" /O2   src/array_sum.c 
 array_sum.exe
--- src/array_sum.c
+++ src/array_sum.c
@@ -5,14 +5,14 @@
 #include "common.h"
 #include "profiler.h"
 
-#define ARRAY_SIZE 1024 * 32
+#define ARRAY_SIZE 1024 * 64
 
 int main()
 {
-    int *a = (int *)malloc(sizeof(int) * ARRAY_SIZE);
-    int *b = (int *)malloc(sizeof(int) * ARRAY_SIZE);
+    int *a = (int *)_aligned_malloc(sizeof(int) * ARRAY_SIZE, 32);
+    int *b = (int *)_aligned_malloc(sizeof(int) * ARRAY_SIZE, 32);
 
-    int *result = (int *)malloc(sizeof(int) * ARRAY_SIZE);
+    int *result = (int *)_aligned_malloc(sizeof(int) * ARRAY_SIZE, 32);
 
     // Initialize a and b.
     for (i32 i = 0; i < ARRAY_SIZE; i++)
@@ -63,8 +63,8 @@ int main()
         for (i32 i = 0; i < ARRAY_SIZE; i += 4)
         {
             // Load 4 elements from a into __m128i's.
-            __m128i simd_a = _mm_set_epi32(a[i + 3], a[i + 2], a[i + 1], a[i]);
-            __m128i simd_b = _mm_set_epi32(b[i + 3], b[i + 2], b[i + 1], b[i]);
+            __m128i simd_a = _mm_load_si128((__m128i *)&a[i]);
+            __m128i simd_b = _mm_load_si128((__m128i *)&b[i]);
 
             // Get the sum and store in result.
             __m128i sum = _mm_add_epi32(simd_a, simd_b);
@@ -87,8 +87,8 @@ int main()
     printf("Element wise array sum cycles : %I64d\n", element_wise_array_sum_counter);
     printf("SIMD array sum cycles : %I64d\n", simd_array_sum_counter);
 
-    free(a);
-    free(b);
+    _aligned_free(a);
+    _aligned_free(b);
 
-    free(result);
+    _aligned_free(result);
 }
